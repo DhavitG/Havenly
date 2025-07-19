f7c425269d2556c0d3171e16234482af048fc380
Dhavit Gandhi <gdhavit@gmail.com>
2025-07-19 16:37:04 +0530

    Featured Destination section

--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../src/Components/Hero";
+import FeaturedDestination from "../src/Components/FeaturedDestination";
 
 function Home() {
   return (
     <>
       <Hero />
+      <FeaturedDestination />
     </>
   );
 }
