bf78a6ae34b867a50322d384b6024b002997a0f0
Dhavit Gandhi <gdhavit@gmail.com>
2025-07-19 13:47:46 +0530

    Hero section of home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
-import {useLocation} from "react-router-dom"
+import {useLocation, Routes, Route} from "react-router-dom"
+import Home from '../pages/Home'
 
 function App() {
   const isOwnerPath = useLocation().pathname.includes("owner")
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70px]">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </div>
     </div>
   )
 }
