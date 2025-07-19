d5bd4617f416795e8f7d1570a3bd47e6b4be5762
Dhavit Gandhi <gdhavit@gmail.com>
2025-07-19 17:30:09 +0530

    Exclusive Offers section

--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../src/Components/Hero";
 import FeaturedDestination from "../src/Components/FeaturedDestination";
+import ExclusiveOffers from "../src/Components/ExclusiveOffers";
 
 function Home() {
   return (
     <>
       <Hero />
       <FeaturedDestination />
+      <ExclusiveOffers />
     </>
   );
 }
