5a40a7718b2ebc33961bd5cb87ff52ddea311c39
Dhavit Gandhi <gdhavit@gmail.com>
2025-07-20 15:53:26 +0530

    Footer Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-import {useLocation, Routes, Route} from "react-router-dom"
-import Home from '../pages/Home'
+import React from "react";
+import Navbar from "./Components/Navbar";
+import { useLocation, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Footer from "../src/Components/Footer";
 
 function App() {
-  const isOwnerPath = useLocation().pathname.includes("owner")
+  const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
       {!isOwnerPath && <Navbar />}
       <div className="min-h-[70px]">
         <Routes>
-          <Route path="/" element={<Home />}/>
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
