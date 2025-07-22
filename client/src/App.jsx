41a6409278bc3f03592836f365165ef518c6cab4
Dhavit Gandhi <gdhavit@gmail.com>
2025-07-22 18:41:32 +0530

    Dashboard Page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,9 @@ import RoomDetails from "./pages/RoomDetails";
 import MyBookings from "./pages/MyBookings";
 import HotelReg from "./Components/HotelReg";
 import Layout from "./pages/hotelOwner/Layout";
+import Dashboard from "./pages/hotelOwner/Dashboard";
+import AddRoom from "./pages/hotelOwner/AddRoom";
+import ListRoom from "./pages/hotelOwner/ListRoom";
 
 function App() {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -22,7 +25,9 @@ function App() {
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="my-bookings" element={<MyBookings />} />
           <Route path="/owner" element={<Layout />}>
-            <Route />
+            <Route index element={<Dashboard />} />
+            <Route path="add-room" element={<AddRoom />} />
+            <Route path="list-room" element={<ListRoom />} />
           </Route>
         </Routes>
       </div>
