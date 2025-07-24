7aaa23af65172ad32d2090ce0b12239c160bef3f
Dhavit Gandhi <gdhavit@gmail.com>
2025-07-24 19:19:43 +0530

    Remainig endpoints of rooms

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
 import upload from "../middlewares/uploadMiddleware.js";
 import { protect } from "../middlewares/authMiddleware.js";
-import { createRoom } from "../controllers/roomController.js";
+import {
+  createRoom,
+  getOwnerRooms,
+  getRoom,
+  toggleRoomAvailability,
+} from "../controllers/roomController.js";
 
 const roomRouter = express.Router();
 
 roomRouter.post("/", upload.array("images", 4), protect, createRoom);
+roomRouter.get("/", getRoom);
+roomRouter.get("/owner", protect, getOwnerRooms);
+roomRouter.post("/toggle-availabilty", protect, toggleRoomAvailability);
 
 export default roomRouter;
