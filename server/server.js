aad117ac584badc8ad3b67e397d17e6c2b4358c3
Dhavit Gandhi <gdhavit@gmail.com>
2025-07-29 18:16:02 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -16,8 +16,24 @@ connectCloudinary();
 
 const app = express();
 
-// Enable cross origin resource sharing (allow any frontend to get connected with this backend )
-app.use(cors());
+// Enable cross origin resource sharing (allow any frontend to get connected with this backend)
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://dhavit-havenly.vercel.app", // replace with actual deployed URL
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 // Middleware
 app.use(clerkMiddleware());
